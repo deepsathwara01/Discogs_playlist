df085fa5e40e50156aeb44b07f17fb8c7b3a31a6
deepsathwara01 <72172951+deepsathwara01@users.noreply.github.com>
2020-11-30 12:35:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React,{ Fragment } from 'react';
 import Discogs from './component/Discogs';
-// import ListOffice from './component/Playlist';
+import ListOffice from './component/Playlist';
 function App() {
   return (
     <Fragment>
       <div className="container">
         <Discogs />
-        {/* <ListOffice /> */}
+         <ListOffice /> 
 
       </div>
     </Fragment>
